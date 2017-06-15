47b2cbe8ec76474ab9e5e4af7e5f5ebb6ed5492a
Srikanth <XChikuX@users.noreply.github.com>
2017-06-15 16:32:43 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+/**
+ * Created by C#iku on 6/15/2017.
+ */
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+
+class App extends Component {
+
+    render()
+    {
+        return (
+            <View>
+                <Text>App pu la</Text>
+            </View>
+        );
+    }
+
+}
+
+export default App;
